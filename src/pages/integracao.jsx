2fb5bf36e3f0065af220aa2c3e32552d7884c315
Mauro Nunes <maurao.dev@gmail.com>
2022-09-05 23:32:54 -0300

    Integração com API adicionado async await

--- src/pages/integracao.jsx
+++ src/pages/integracao.jsx
@@ -5,10 +5,11 @@ export default function Integracao() {
   const [codigo, setCodigo] = useState(1);
   const [cliente, setCliente] = useState({});
 
-  function obterCliente() {
-    fetch("http://localhost:3000/api/clientes/" + codigo)
-      .then((resp) => resp.json())
-      .then((dados) => setCliente(dados));
+  async function obterCliente() {
+    const resp = await fetch("http://localhost:3000/api/clientes/" + codigo);
+    const dados = await resp.json();
+
+    setCliente(dados);
   }
 
   return (
